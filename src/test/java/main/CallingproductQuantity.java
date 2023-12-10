0cd59c08cca122f74f9842bf1a6f3775b457f1d8
sainath <sainath@LAPTOP-86HTKKRD>
2023-12-10 23:28:45 +0530

    added a class callingproduct

--- src/test/java/main/CallingproductQuantity.java
+++ src/test/java/main/CallingproductQuantity.java
@@ -1,6 +1,29 @@
 package main;
 
-public class CallingproductQuantity {
+import java.time.Duration;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import main.Module.ViewProductquntity;
+
+public class CallingproductQuantity {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
+	
+		ViewProductquntity prodq = new ViewProductquntity(driver);
+		prodq.uRL();
+		prodq.isHomePageVisible();
+		prodq.productM();
+		prodq.performActionWithScrolling();
+		prodq.scrollDown();
+	
+	
+	}	
 
 }
